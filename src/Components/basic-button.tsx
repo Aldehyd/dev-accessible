929ffc8f7690d4fd7856b8e88de21039976b1dfd
Hugues Labat <labat.hugues@gmail.com>
2024-03-15 14:04:46 +0100

    add basic buttons contrast version

--- src/Components/basic-button.tsx
+++ src/Components/basic-button.tsx
@@ -7,9 +7,13 @@ export default function BasicButton({text,disableAbility=false,disabledStatus="f
         <div className={basicButtonClassNames}>
             {
             disableAbility ? 
-                <button className="basic-button" aria-disabled={disabledStatus}>{text}</button>
+                <button className="basic-button" aria-disabled={disabledStatus}>
+                    <span className="basic-button_text">{text}</span>
+                </button>
                 :
-                <button className="basic-button">{text}</button>
+                <button className="basic-button">
+                    <span className="basic-button_text">{text}</span>
+                </button>
             }
             <div className="basic-button-shadow"></div>
         </div>
