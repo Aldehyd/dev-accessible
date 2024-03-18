03da721a50dc14e8f35aa251d71b6ee9e04fa47a
Hugues Labat <labat.hugues@gmail.com>
2024-03-18 18:55:48 +0100

    complete language select

--- src/Components/language-select.tsx
+++ src/Components/language-select.tsx
@@ -10,6 +10,10 @@ export default function LanguageSelect(): React.JSX.Element {
     const [topClassList,setTopClassList] = useState("language-select_top");
     const [centerClassList,setCenterClassList] = useState("language-select_center");
 
+    const languageSelect = useRef(null);
+    const languageSelectTop = useRef(null);
+    const languageSelectCenter = useRef(null);
+
     const [expanded,setExpanded] = useState<string>("false");
 
     const handleClickOnSelect = ()=> {
@@ -23,28 +27,48 @@ export default function LanguageSelect(): React.JSX.Element {
         };
     };
 
+    const closeOnClickAnywhereElse = (e)=> {
+        !languageSelect.current?.contains(e.target) && setExpanded("false");
+    };
+
+    const handleKeyDown = (e)=> {
+        switch(e.key) {
+            case "Escape":
+                setExpanded("false");
+                break;
+            default:
+                break;
+        };
+    };
+
     useEffect(()=> {
+        document.documentElement.setAttribute('lang',language === "french" ? "fr" : "en");
         setSelectClassList(`language-select ${language === "french" ? "fr" : "en"}`);
         setTopClassList(`language-select_top ${language === "french" ? "fr" : "en"}`);
         setCenterClassList(`language-select_center ${language === "french" ? "en" : "fr"}`);
     },[language]);
     
-    const languageSelectTop = useRef(null);
-    const languageSelectCenter = useRef(null);
+    useEffect(()=> {
+        window.addEventListener('click',(e)=> closeOnClickAnywhereElse(e));
+            
+        return() => {window.removeEventListener('click',closeOnClickAnywhereElse)}
+    },[]);
 
     return(
-        <div className={selectClassList} aria-expanded={expanded} role="listbox" aria-label="language choice">
+        <div className={selectClassList} ref={languageSelect} aria-expanded={expanded} 
+            role="listbox" aria-label={language === "french" ? "choix de la langue" : "language choice"} 
+            onKeyDown={(e)=> handleKeyDown(e)} tabIndex="0">
             <div ref={languageSelectTop} className={topClassList} onClick={()=> handleClickOnSelect()}>
                 <span className="language-select_top_fr" role="option">FR</span>
                 <span className="language-select_top_en" role="option">EN</span>
             </div>
             <div ref={languageSelectCenter} className={centerClassList}>
                 <span className="language-select_center_fr" role="option"
-                    onClick={()=> handleClickOnOption("french")}>
+                    onClick={()=> handleClickOnOption("french")} aria-label="français" lang="fr">
                     FR
                 </span>
                 <span className="language-select_center_en" role="option"
-                onClick={()=> handleClickOnOption("english")}>
+                onClick={()=> handleClickOnOption("english")} aria-label="english" lang="en">
                     EN
                 </span>
             </div>
