af746dacb74cc8cf302a3e5b909d8a3607af9cd1
Hugues Labat <labat.hugues@gmail.com>
2024-03-19 12:31:01 +0100

    improve basic button component (on white bg & on click function)

--- src/Components/basic-button.tsx
+++ src/Components/basic-button.tsx
@@ -1,22 +1,25 @@
 interface BasicButtonPropsInterface {
     text: string,
-    disableAbility: boolean,
-    disabledStatus: string
+    disableAbility?: boolean,
+    disabledStatus?: string,
+    onWhiteBackground?: boolean,
+    onClickFunction?: ()=> void
 }
 
-export default function BasicButton({text,disableAbility=false,disabledStatus="false"}: BasicButtonPropsInterface): React.JSX.Element {
+export default function BasicButton({text,disableAbility=false,disabledStatus="false",onWhiteBackground=false,onClickFunction}: BasicButtonPropsInterface): React.JSX.Element {
     
-    let basicButtonClassNames = `basic-button-container ${disabledStatus==="true" ? "disabled" : ""}`;
+    let basicButtonClassNames = `basic-button-container ${onWhiteBackground && "basic-button-container--on-white-background"} ${disabledStatus==="true" ? "disabled" : ""}`;
 
     return(
         <div className={basicButtonClassNames}>
             {
             disableAbility ? 
-                <button className="basic-button" aria-disabled={disabledStatus}>
+                <button className="basic-button" aria-disabled={disabledStatus}
+                    onClick={onClickFunction}>
                     <span className="basic-button_text">{text}</span>
                 </button>
                 :
-                <button className="basic-button">
+                <button className="basic-button" onClick={onClickFunction}>
                     <span className="basic-button_text">{text}</span>
                 </button>
             }
