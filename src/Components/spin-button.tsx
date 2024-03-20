e8845ac0f1c699b086d64e9c98a132c158415dc8
Hugues Labat <labat.hugues@gmail.com>
2024-03-20 14:04:22 +0100

    fix spin button with language choice

--- src/Components/spin-button.tsx
+++ src/Components/spin-button.tsx
@@ -52,11 +52,11 @@ export default function SpinButton({name,frenchLabel,englishLabel,minValue = 0,m
         <div className="font-size-spin-button-container">
             <label id="spin-button-label">{language === "french" ? frenchLabel : englishLabel} : </label>
             <div className="spin-button-container">
-                <BasicButton text="-" tabIndex="-1" disableAbility={true} disabledStatus={currentValue <= minValue ? "true" : "false"} onWhiteBackground={true} onClickFunction={()=>decrease(input,setCurrentValue,effectFunction)} />
+                <BasicButton frenchText="-" englishText="-" tabIndex="-1" disableAbility={true} disabledStatus={currentValue <= minValue ? "true" : "false"} onWhiteBackground={true} onClickFunction={()=>decrease(input,setCurrentValue,effectFunction)} />
                 <span className="input-container">  
                     <input type="text" ref={input} onChange={(e)=> handleChange(e)} onKeyDown={(e)=> handleKeyDown(e,input,setCurrentValue,minValue,maxValue,effectFunction)} maxLength={maxInputLength} className="spin-button_input" role="spinbutton" defaultValue={defaultValue} aria-valuenow={currentValue} aria-valuemin={minValue} aria-valuemax={maxValue} aria-valuetext={currentValue + "%"} aria-invalid={currentValue > maxValue || currentValue < minValue ? "true" : "false"} aria-labelledby="spin-button-label" />
                 </span>
-                <BasicButton text="+" tabIndex="-1" disableAbility={true} disabledStatus={currentValue >= maxValue ? "true" : "false"} onWhiteBackground={true} onClickFunction={()=>increase(input,setCurrentValue,effectFunction)} />
+                <BasicButton frenchText="+" englishText="+" tabIndex="-1" disableAbility={true} disabledStatus={currentValue >= maxValue ? "true" : "false"} onWhiteBackground={true} onClickFunction={()=>increase(input,setCurrentValue,effectFunction)} />
             </div>
         </div>
     )
