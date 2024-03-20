386215aed8958253278007afd8d0ca73370bdb77
Hugues Labat <labat.hugues@gmail.com>
2024-03-20 13:03:58 +0100

    refactor search bar css

--- src/Components/search-bar.tsx
+++ src/Components/search-bar.tsx
@@ -1,18 +1,47 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import LanguageContext from "../Contexts/language-context.tsx";
 
 export default function SearchBar(): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
+    const input = useRef(null);
+
+    const [isSearching,setIsSearching] = useState<boolean>(false);
+
+    const classList = `search-bar-container ${isSearching ? 'searching' : ''}`;
+
+    const handleKeyDown = (e: KeyboardEvent)=> {
+        switch(e.key) {
+            case ' ':
+                e.preventDefault();
+                break;
+            case 'Enter': 
+                this.launchSearch();
+                break;
+            default:
+                break;
+        };
+    };
+
+    const launchSearch = ()=> {
+        if(input.current?.value.length > 0) {
+            setIsSearching(true);
+            setTimeout(()=> setIsSearching(false),1000);
+            // this.analyseSearchWords(this.input.value);
+        };
+    };
 
     return(
-        <div class="search-bar-container animations top-menu_item">
-            <span class="input-container">
-                <input type="text" placeholder="Rechercher..." maxlength="26" />
+        <div className={classList}>
+            <span className="input-container">
+                <input type="text" placeholder={language === "french" ? "Rechercher..." : "Search..."}
+                 maxLength="26" onKeyDown={(e)=> handleKeyDown(e)} ref={input} />
             </span>
-            <button type="button" class="search-button">
-                <span class="circle"></span>
-                <span class="rectangle"></span>
+            <button type="button" className="search-button" 
+                aria-label={language === "french" ? "Rechercher" : "Search"}
+                onClick={()=> launchSearch()}>
+                <span className="circle"></span>
+                <span className="rectangle"></span>
             </button>
         </div>
     )
