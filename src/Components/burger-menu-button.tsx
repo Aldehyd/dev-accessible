7297d42a6f820180511ae1a2d56bd171dbb29320
Hugues Labat <labat.hugues@gmail.com>
2024-03-20 13:33:46 +0100

    complete burger menu button

--- src/Components/burger-menu-button.tsx
+++ src/Components/burger-menu-button.tsx
@@ -4,9 +4,10 @@ import LanguageContext from "../Contexts/language-context.tsx";
 export default function BurgerMenuButton(): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
-    
+
     return(
-        <button className="main-burger-menu" aria-label={language === "french" ? "principal" : "main"}>
+        <button className="main-burger-menu" aria-label={language === "french" ? "principal" : "main"}
+            aria-haspopup="menu">
             <span className="main-burger-menu_line--1"></span>
             <span className="main-burger-menu_line--2"></span>
             <span className="main-burger-menu_line--3"></span>
