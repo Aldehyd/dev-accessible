0ee29b47b7d3b9302fc59955ab421591195e7c78
Hugues Labat <labat.hugues@gmail.com>
2024-03-22 17:02:49 +0100

    start to write achievments

---
+++ src/Datas/achievments.tsx
@@ -0,0 +1,49 @@
+
+export const achievments = [
+    {
+        title: "dyskredy-art.com",
+        websiteLink: "https://dykredy-art.com",
+        githubLink: "bla",
+        imageName: "dyskredy-preview",
+        frenchAlt: "très beau site",
+        englishAlt: "beautiful website",
+        frenchType: "Site internet",
+        englishType: "Website",
+        frenchNeed: "Site artistique présentant des galeries d'illustrations, de photos et de vidéos.",
+        englishNeed: "Website with draws, photos and videos galeries.",
+        developmentTools: ["VsCode", "NPM", "Webpack"],
+        technologies: {
+            frontEnd: ["HTML", "ARIA", "CSS", "TYPESCRIPT"],
+            backEnd: ["NODEJS"]
+        },
+        accessibility: {
+            score: 97,
+            frenchNonCompliance: ["Restitution non évaluée avec le lecteur d'écran Jaws","vidéo ne possédant pas d'une audio description"],
+            englishNonCompliance: ["Reading with Jaws screen readers not tested","vidéo with no audiodescription"]
+        },
+        frenchFonctionnalities: ["responsive","thème alternatif contrasté","activation ou non des animations","réglages de la police, de sa taille et des espacements","multilangues (français/anglais)","lecteur vidéo","caroussel","formulaire de contact"],
+        englishFonctionnalities: ["responsive","alternative contrasted theme","enable or not animations","font, font size and spacings settings","multilanguages (french/english)","video player","carousel","contact form"],
+        design: {
+            frenchWork: ["Design général", "bannière", "composants", "page 404"],
+            englishWork: ["Global design", "banner", "components", "404 page"],
+            tools: ["Adobe Illustrator"]
+        },
+        frenchDeployment: "Serveur mutualisé sur PlanetHoster",
+        englishDeployment: "Common server on PlanetHoster",
+        frenchComments: "Tous les composants ont été codés intégralement (aucune librairie utilisée) avec des classes ES6.",
+        englishComents: "All the components have been coded from scratch (no library used) with ES6 classes.",
+        pictures: [
+            {
+                pictureName: "dyskredy-preview",
+                frenchAlt: "",
+                englishAlt: "",
+            },
+            {
+                pictureName: "guitare",
+                frenchAlt: "",
+                englishAlt: "",
+            }
+        ]
+
+    }
+]
