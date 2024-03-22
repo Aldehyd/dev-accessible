76edf2058aca7405e091e7cf192230d3a3452e03
Hugues Labat <labat.hugues@gmail.com>
2024-03-22 11:19:32 +0100

    add video to Datas

---
+++ src/Datas/video.tsx
@@ -0,0 +1,9 @@
+
+export const video = {
+    videoURL: '../Videos/guitare.mp4',
+    pictureURL: '../Pictures/guitare.JPG',
+    frenchTitle: 'Titre de la vidéo',
+    englishTitle: 'Video Title',
+    frenchTranscription: 'Voici la transcription française de la vidéo',
+    englishTranscription: 'Here is the english transcription of the video'
+};
