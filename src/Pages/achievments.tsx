ef53efad12be72c943c42c7f7e9d0b411b330ff6
Hugues Labat <labat.hugues@gmail.com>
2024-03-25 14:19:24 +0100

    progress on achievments page

--- src/Pages/achievments.tsx
+++ src/Pages/achievments.tsx
@@ -1,8 +1,22 @@
 import { achievments } from "../Datas/achievments.tsx";
 import AchievmentsMain from "../Layout/achievmentsMain.tsx";
+import AnimationToggleButton from "../Components/animation-toggle-button.tsx";
+import LanguageSelect from "../Components/language-select.tsx";
+import Switch from "../Components/switch.tsx";
+import RadioButtonGroup from "../Components/radio-button.tsx";
+import {handleContrastedThemeSwitch} from "../Functions/handleContrastedThemeSwitch.tsx";
+import {handleMainColor} from "../Functions/handleMainColor.tsx";
+import {colorsRadioButtonsData} from "../Datas/colorsRadioButtonsData.tsx";
 
 export default function Achievments(): React.JSX.Element {
     return (
-        <AchievmentsMain achievments={achievments} />
+        <>
+            <AnimationToggleButton />
+            <LanguageSelect />
+            <Switch name="contrasted-theme" frenchLabel="Thème contrasté" englishLabel="Contrasted theme" onSwitchFunction={handleContrastedThemeSwitch} />
+            <RadioButtonGroup name="color" frenchLegend="Couleur" englishLegend="Color" radioButtonsData={colorsRadioButtonsData} handleChoice={handleMainColor} />
+            <AchievmentsMain achievments={achievments} />
+        </>
+        
     )
 }
