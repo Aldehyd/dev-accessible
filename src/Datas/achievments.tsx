47cbabf2ea536b95f2a4d502326f5eb9d49c3093
Hugues Labat <labat.hugues@gmail.com>
2024-03-25 13:21:55 +0100

    complete achievments datas

--- src/Datas/achievments.tsx
+++ src/Datas/achievments.tsx
@@ -14,8 +14,9 @@ export const achievments = [
         year: "2023",
         developmentTools: ["VsCode", "NPM", "Webpack"],
         technologies: {
-            frontEnd: ["HTML", "ARIA", "CSS", "TYPESCRIPT"],
-            backEnd: ["NODEJS"]
+            frontEnd: ["HTML", "ARIA", "CSS", "TypeScript"],
+            backEnd: ["NodeJS"],
+            dataBase : []
         },
         accessibility: {
             score: 97,
@@ -46,5 +47,99 @@ export const achievments = [
             }
         ]
 
+    },
+    {
+        title: "dev-accessible.com",
+        websiteLink: "https://dev-accessible.com",
+        githubLink: "bla",
+        imageName: "guitare",
+        frenchAlt: "très beau site",
+        englishAlt: "beautiful website",
+        frenchType: "Site internet",
+        englishType: "Website",
+        frenchNeed: "Site professionnel de développeur web regroupant CV et réalisations.",
+        englishNeed: "Web developper website with CV and achievments.",
+        year: "2024",
+        developmentTools: ["VsCode", "NPM", "Git"],
+        technologies: {
+            frontEnd: ["HTML", "ARIA", "CSS", "SASS", "ReactJS", "TypeScript"],
+            backEnd: ["NODEJS"],
+            dataBase: ["MongoDB"]
+        },
+        accessibility: {
+            score: 0,
+            frenchNonCompliance: ["Restitution non évaluée avec le lecteur d'écran Jaws","vidéo ne possédant pas d'une audio description"],
+            englishNonCompliance: ["Reading with Jaws screen readers not tested","vidéo with no audiodescription"]
+        },
+        frenchFonctionnalities: ["responsive","thème alternatif contrasté","moteur de recherche","activation ou non des animations","réglages de la police, de sa taille et des espacements","multilangues (français/anglais)","lecteur vidéo","caroussel","API de requêtes vers base de données","formulaire de contact"],
+        englishFonctionnalities: ["responsive","alternative contrasted theme", "search engine","enable or not animations","font, font size and spacings settings","multilanguages (french/english)","video player","carousel","API calls to database","contact form"],
+        design: {
+            frenchWork: ["Design général", "composants", "icones", "vidéos de présentation"],
+            englishWork: ["Global design", "components", "icons", "presentation video"],
+            tools: ["Figma","Adobe Illustrator","Adobe AfterEffects", "Adobe PremierePro"]
+        },
+        frenchDeployment: "Serveur mutualisé sur PlanetHoster",
+        englishDeployment: "Common server on PlanetHoster",
+        frenchComments: "Tous les composants ont été codés intégralement (aucune librairie utilisée).",
+        englishComents: "All the components have been coded from scratch (no library used).",
+        pictures: [
+            {
+                pictureName: "dyskredy-preview",
+                frenchAlt: "",
+                englishAlt: "",
+            },
+            {
+                pictureName: "guitare",
+                frenchAlt: "",
+                englishAlt: "",
+            }
+        ]
+    },
+    {
+        title: "shopping",
+        websiteLink: "google play store",
+        githubLink: "bla",
+        imageName: "guitare",
+        frenchAlt: "très belle application",
+        englishAlt: "beautiful application",
+        frenchType: "Application mobile",
+        englishType: "Mobile application",
+        frenchNeed: "Création et édition de listes de courses à partir de magasins personnalisés.",
+        englishNeed: "Creating and editing shop list from custom hops.",
+        year: "2024",
+        developmentTools: ["VsCode", "AndroidStudio"],
+        technologies: {
+            frontEnd: ["React Native"],
+            backEnd: [],
+            dataBase: []
+        },
+        accessibility: {
+            score: 0,
+            frenchNonCompliance: ["Non évaluée"],
+            englishNonCompliance: ["No evaluation"]
+        },
+        frenchFonctionnalities: ["thème colorés alternatifs","gestion du backhandler","stockage des données dans le téléphone"],
+        englishFonctionnalities: ["colored themes","backhandler management","local storage of datas"],
+        design: {
+            frenchWork: ["Design général", "composants"],
+            englishWork: ["Global design", "components"],
+            tools: ["Figma"]
+        },
+        frenchDeployment: "Google Play Store",
+        englishDeployment: "Google Play Store",
+        frenchComments: "",
+        englishComents: "",
+        pictures: [
+            {
+                pictureName: "dyskredy-preview",
+                frenchAlt: "",
+                englishAlt: "",
+            },
+            {
+                pictureName: "guitare",
+                frenchAlt: "",
+                englishAlt: "",
+            }
+        ]
     }
 ]
