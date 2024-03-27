67d3103b7be0f1346e35e0246edb4bd856f212a4
Hugues Labat <labat.hugues@gmail.com>
2024-03-27 15:16:52 +0100

    create diplomas datas

---
+++ src/Datas/diplomas.tsx
@@ -0,0 +1,48 @@
+export const diplomas = [
+    {
+       id: 'access',
+       frenchTitle: 'Certification accessibilité numérique',
+       englishTitle: 'Web accessibility certification',
+       list: [
+          {
+             frenchTitle: 'Certification \'Développer des sites web accessibles\'',
+             englisTitle : '\'Developp accessible websites\' certification',
+             frenchDate: 'Septembre 2023',
+             englishDate: 'September 2023',
+             frenchInstitute: 'Access42',
+             englishInstitute: 'Access42',
+             score: '97%'
+          }
+       ]
+    },
+    {
+       id: 'engineer',
+       frenchTitle: 'Ingénieur Génie Civil',
+       englishTitle: 'Civil Engineering',
+       list: [
+          {
+             frenchTitle: 'Diplôme d\'ingénieur en Génie Civil, option Développement durable',
+             englishTitle : 'Civil Engineering graduate, sustainable development option',
+             frenchDate: 'Septembre 2013',
+             englishDate: 'September 2013',
+             frenchInstitute: 'Polytech\' Marseille',
+             englishInstitute: 'Polytech\' Marseille'
+          }
+       ]
+    },
+    {
+       id: 'deug',
+       frenchTitle: 'DEUG Mathématiques Informatique',
+       englishTitle: 'Maths Computer science',
+       list: [
+          {
+             frenchTitle: 'DEUG Mathématiques Informatique',
+             englishTitle : 'Maths & Computer science Degree',
+             frenchDate: 'Septembre 2010',
+             englishDate: 'September 2010',
+             frenchInstitute: 'Université de Provence - Marseille',
+             englishInstitute: 'Provence University - Marseille',
+          }
+       ]
+    }
+ ]
