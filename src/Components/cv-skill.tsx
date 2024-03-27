29e3085d146865f216d12472ee3acb51740b4815
Hugues Labat <labat.hugues@gmail.com>
2024-03-27 16:00:32 +0100

    implement info dialog layout in cv skill component

--- src/Components/cv-skill.tsx
+++ src/Components/cv-skill.tsx
@@ -1,17 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import LanguageContext from "../Contexts/language-context.tsx";
+import InfosDialog from "../Layout/infosDialog.tsx";
 
 interface CVSkillPropsInterface {
     skill: any,
+    type: string
 }
 
-export default function CVSkill({skill}: CVSkillPropsInterface): React.JSX.Element {
+export default function CVSkill({skill,type}: CVSkillPropsInterface): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
 
+    const [displayInfoDialog,setDisplayInfoDialog] = useState<boolean>(false);
+
     return (
-        <button className="cv_skill" aria-expanded={ariaExpanded} aria-haspopup='true'>
-            {language === 'french' ? skill.frenchTitle : skill.englishTitle}
-        </button>
+        <div className="cv_skill-container">
+            <button className="cv_skill" aria-expanded={displayInfoDialog ? "true" : "false"} aria-haspopup='true'>
+                {language === 'french' ? skill.frenchTitle : skill.englishTitle}
+            </button>
+            {displayInfoDialog && <InfosDialog skill={skill} type={type} setDisplayInfoDialog={setDisplayInfoDialog} />}
+        </div>
     )
 }
