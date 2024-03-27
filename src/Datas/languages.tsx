c0d5bd2d00cf7f7a530511aebe334d42c319d0b2
Hugues Labat <labat.hugues@gmail.com>
2024-03-27 15:18:03 +0100

    create languages datas

---
+++ src/Datas/languages.tsx
@@ -0,0 +1,25 @@
+export const languages = [
+    {
+        id: 'english',
+        frenchTitle: 'Anglais (niveau TOEIC)',
+        englishTitle: 'English (TOEIC level)',
+        list: [
+           {
+              frenchTitle: 'TOEIC passé en 2012',
+              englishTitle: 'TOEIC passed in 2012',
+              score: '925/990'
+           }
+        ]
+     },
+     {
+        id: 'lsf',
+        frenchTitle: 'Langue des Signes',
+        englishTitle: 'French Sign language',
+        list: [
+           {
+              frenchTitle: 'Apprentissage autodidacte de la Langue des Signes Française',
+              englishTitle: 'Self-learning of the french signs language',
+           }
+        ]
+     }
+]
