dda500bd44cc5dbb5923d86983c39c90f7841264
Hugues Labat <labat.hugues@gmail.com>
2024-03-27 15:48:27 +0100

    create infoDialog layout

---
+++ src/Layout/infosDialog.tsx
@@ -0,0 +1,52 @@
+import {useContext} from 'react';
+import CloseButton from "../Components/close-button.tsx";
+import LanguageContext from "../Contexts/language-context.tsx";
+
+interface InfosDialogPropsInterface {
+    skill: any,
+};
+
+export default function InfosDialog({skill,type,setDisplayInfoDialog}: InfosDialogPropsInterface): React.JSX.Element {
+    
+    const {language} = useContext(LanguageContext);
+
+    return (
+        <div ref={infoDialog} style={{top:dialogVerticalPosition,left:dialogLeftPosition}} className="infos-dialog" role='dialog' aria-labelledby='dialog-title'>
+                <CloseButton onClickFunction={()=> setDisplayInfoDialog(false)} />
+                <div ref={infoDialogTop} className="infos-dialog_top">
+                </div>
+                <div ref={centerDialog} className="infos-dialog_center">
+                    <div ref={dialogContent} className='infos-dialog_content-container'>
+                        <div className="infos-dialog_content">
+                            <h3 id='dialog-title'>{language === 'french' ? skill.frenchTitle : skill.englishTitle}</h3>
+                            <p>{language === 'french' ? skill.frenchDescription : skill.englishDescription}</p>
+                            <ul className="infos-dialog_list">
+                                {
+                                    type === "diploma" && skill.list.map(listItem =>
+                                        <li key={listItem.id} class="projects-list_list-item">
+                                            <dl>
+                                                <dt>{language === "french" ? "Titre" : "Title"} :</dt>
+                                                <dd>{language === 'french' ? listItem.frenchTitle : listItem.englishTitle}</dd>
+
+                                                <dt>Date :</dt>
+                                                <dd>{language === 'french' ? listItem.frenchDate : listItem.englishDate}</dd>
+
+                                                <dt>{language === 'french' ? "Organisme de formation" : "Formation institute"} :</dt>
+                                                <dd>{language === "french" ? listItem.frenchInstitute : listItem.englishInstitute}</dd>
+                                                
+                                                {listItem.score && 
+                                                    <>
+                                                        <dt>Score :</dt>
+                                                        <dd>{listItem.score}</dd>
+                                                    </>
+                                                }   
+                                            </dl>
+                                        </li>)
+                                }   
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div ref={infoDialogBottom} className="infos-dialog_bottom"></div>
+    )
+}
