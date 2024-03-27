df0b68cccc6bc0a8d05c852c770f7c14d51cd82e
Hugues Labat <labat.hugues@gmail.com>
2024-03-27 15:14:22 +0100

    add language context to cv skill component

--- src/Components/cv-skill.tsx
+++ src/Components/cv-skill.tsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import LanguageContext from "../Contexts/language-context.tsx";
 
 interface CVSkillPropsInterface {
     skill: any,
 }
 
 export default function CVSkill({skill}: CVSkillPropsInterface): React.JSX.Element {
+
+    const {language} = useContext(LanguageContext);
+
     return (
         <button className="cv_skill" aria-expanded={ariaExpanded} aria-haspopup='true'>
             {language === 'french' ? skill.frenchTitle : skill.englishTitle}
