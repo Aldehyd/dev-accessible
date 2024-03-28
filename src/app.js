64329cc52b80cff639b662c391b6fa55f2fafc93
Hugues Labat <labat.hugues@gmail.com>
2024-03-28 17:37:43 +0100

    add mongo driver to app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongo = require("mongodb");
 
 const app = express();
 const port = 4000;
@@ -7,3 +10,32 @@ app.listen(port, ()=> {
     console.log("server successfully started");
 });
 
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('hello');
+});
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.STRING_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
