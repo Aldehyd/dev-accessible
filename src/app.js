0365ebe4432f4d2e35f82030f2320316719ffa53
Hugues Labat <labat.hugues@gmail.com>
2024-03-29 11:21:04 +0100

    add api routes to app

--- src/app.js
+++ src/app.js
@@ -2,22 +2,11 @@ require('dotenv').config();
 
 const express = require("express");
 const mongo = require("mongodb");
-
-const app = express();
-const port = 4000;
-
-app.listen(port, ()=> {
-    console.log("server successfully started");
-});
-
-app.use(express.json());
-
-app.get('/',(req,res)=>{
-    res.send('hello');
-});
+const cors = require("cors");
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.STRING_URI;
+const dbName = process.env.STRING_DBNAME;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,16 +15,35 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const app = express();
+const port = process.env.PORT ||4000;
+
+app.listen(port, ()=> {
+    console.log("server successfully started");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+});
+
+app.get('/cv',async(req,res)=>{
+    await client.connect((err,db)=> {
+        console.log("successfully connected to db")
+        if(err || !db) {
+            return false;
+        };
+        db.db(dbName).collection("achievments").find().toArray(
+            function(err,results) {
+                if(!err) {
+                    res.status(200).send(results);
+                };
+            }
+        );
+    });
+});
+
+
