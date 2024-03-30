3431b4818ca27240a3aa5e0eec55a27fc37da3f2
Hugues Labat <labat.hugues@gmail.com>
2024-03-30 16:11:47 +0100

    create error smiley component

---
+++ src/Components/error-smiley.tsx
@@ -0,0 +1,11 @@
+export default function ErrorSmiley(): React.JSX.Element {
+    return (
+        <div className="error_smiley">
+            <div className="error_smiley_eyes">
+                <div className="error_smiley_left-eye"></div>
+                <div className="error_smiley_right-eye"></div>
+            </div>
+            <div className="error_smiley_mouth"></div>
+        </div>
+    )
+}
