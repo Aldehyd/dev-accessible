f142112124806723677b8b2fb70f7e86a5f83b3c
Hugues Labat <labat.hugues@gmail.com>
2024-03-31 18:39:39 +0200

    add available status to cv component

--- src/Layout/cvComponent.tsx
+++ src/Layout/cvComponent.tsx
@@ -4,18 +4,20 @@ import CVSection from "./cvSection.tsx";
 import AchievmentInterface from "../Interfaces/achievmentInterface.tsx";
 import DiplomaInterface from "../Interfaces/diplomaInterface.tsx";
 import LanguageInterface from "../Interfaces/languageInterface.tsx";
-
+import AvailableStatusIndicator from "../Components/availableStatusIndicator.tsx";
 interface CVComponentPropsInterface {
+    availableStatus: boolean,
     achievments: AchievmentInterface[],
     diplomas: DiplomaInterface[],
     languages: LanguageInterface[]
 }
 
-export default function CVComponent({achievments,diplomas,languages}: CVComponentPropsInterface): React.JSX.Element {
+export default function CVComponent({availableStatus,achievments,diplomas,languages}: CVComponentPropsInterface): React.JSX.Element {
     console.log(achievments)
     return (
         <div className="cv">
             <CVMainLine />
+            <AvailableStatusIndicator isAvailable={availableStatus} />
             <CVSection type="technology" achievments={achievments} diplomas={diplomas} languages={languages} />
             <CVSection type="devTool" achievments={achievments} diplomas={diplomas} languages={languages} />
             <CVSection type="design" achievments={achievments} diplomas={diplomas} languages={languages} />
