b5fc1196e4818670551bd20928d2c08c295157c1
Hugues Labat <labat.hugues@gmail.com>
2024-04-04 15:27:50 +0200

    complete accessibility settings button

--- src/Components/accessibility-settings-button.tsx
+++ src/Components/accessibility-settings-button.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import LanguageContext from "../Contexts/language-context.tsx";
-import ModalContext from "../Contexts/modal-context.tsx";
 
-export default function AccessibilitySettingsButton(): React.JSX.Element {
+interface AccessibilitySettingsButtonPropsInterface {
+    setDisplay: (display: boolean)=> void
+}
+
+export default function AccessibilitySettingsButton({setDisplay}: AccessibilitySettingsButtonPropsInterface): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
 
     return(
-        <button type="button" className="accessibility-settings-button" onClick={()=> changeIsModalDisplayed(true)}>
+        <button type="button" className="accessibility-settings-button" onClick={()=> setDisplay(true)}>
             <span className="wheel-symbol"></span>
             {language === "french" ? "Accessibilité" : "Accessibility"}
         </button>
