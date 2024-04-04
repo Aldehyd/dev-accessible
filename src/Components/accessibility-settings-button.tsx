93498de9747aca8dec59148bf6ed6286eac97445
Hugues Labat <labat.hugues@gmail.com>
2024-04-04 14:25:49 +0200

    create top menu

--- src/Components/accessibility-settings-button.tsx
+++ src/Components/accessibility-settings-button.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import LanguageContext from "../Contexts/language-context.tsx";
+import ModalContext from "../Contexts/modal-context.tsx";
 
 export default function AccessibilitySettingsButton(): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
 
     return(
-        <button type="button" className="accessibility-settings-button">
+        <button type="button" className="accessibility-settings-button" onClick={()=> changeIsModalDisplayed(true)}>
             <span className="wheel-symbol"></span>
             {language === "french" ? "Accessibilité" : "Accessibility"}
         </button>
--- src/Layout/topMenu.tsx
+++ src/Layout/topMenu.tsx
@@ -2,13 +2,14 @@ import AnimationToggleButton from "../Components/animation-toggle-button.tsx";
 import LanguageSelect from "../Components/language-select.tsx";
 import AccessibilityAnalysisIndicator from "../Components/accessibility-analysis-indicator.tsx";
 import SearchBar from "../Components/search-bar.tsx";
+import AccessibilitySettingsButton from "../Components/accessibility-settings-button.tsx";
 
 export default function TopMenu(): React.JSX.Element {
 
     return (
         <div className="top-menu">
             <div className="top-menu_buttons-invisible-container">
-
+                <AccessibilitySettingsButton />
                 <AnimationToggleButton />
                 <LanguageSelect />
                 <AccessibilityAnalysisIndicator />
