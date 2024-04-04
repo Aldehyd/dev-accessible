ff3fb2668013a7949a9dff0d5185d872ba792de1
Hugues Labat <labat.hugues@gmail.com>
2024-04-04 15:29:36 +0200

    complete modal layout

--- src/Modals/modalLayout.tsx
+++ src/Modals/modalLayout.tsx
@@ -3,7 +3,14 @@ import ModalContext from "../Contexts/modal-context.tsx";
 import LanguageContext from "../Contexts/language-context.tsx";
 import CloseButton from "../Components/close-button.tsx";
 
-export default function ModalLayout({children}): React.JSX.Element {
+interface ModalLayoutPropsInterface {
+    setDisplay: (display: boolean)=> void,
+    frenchTitle: string,
+    englishTitle: string,
+    children: any
+}
+
+export default function ModalLayout({setDisplay,frenchTitle,englishTitle,children}: ModalLayoutPropsInterface): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
     const {isModalDisplayed,changeIsModalDisplayed} = useContext(ModalContext);
@@ -14,11 +21,21 @@ export default function ModalLayout({children}): React.JSX.Element {
     },[]);
 
     return (
-        <div className="modal modal--accessibility-settings">
-            <div className="modal_top-focus-trap"></div>
-            <CloseButton onClickFunction={()=> changeIsModalDisplayed(false)}/>
-            {children}
-            <div className="modal_bottom-focus-trap"></div>
+        <div className="modal-layout">
+            <div className="modal-layout_top">
+                <div className="modal-layout_top-focus-trap"></div>
+                <h1 className="modal-layout_title">
+                    {language === "french" ? frenchTitle : englishTitle}
+                </h1>
+                <CloseButton onWhiteBackground={true} onClickFunction={()=> setDisplay(false)}/>
+            </div>
+            <div className="modal-layout_center">
+                <div className="modal-layout_content">
+                    {children}
+                </div>
+                <div className="modal-layout_bottom-focus-trap"></div>
+            </div>
+            <div className="modal-layout_bottom"></div>
         </div>
     )
 }
