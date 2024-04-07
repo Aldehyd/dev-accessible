05bef8be22dea2f8db90890e86834fe5bfea1181
Hugues Labat <labat.hugues@gmail.com>
2024-04-07 07:09:43 +0200

    modify cv picture

--- src/Layout/cvHeadSection.tsx
+++ src/Layout/cvHeadSection.tsx
@@ -11,7 +11,7 @@ export default function CVHeadSection({availableStatus}): React.JSX.Element {
             <h1>{language === "french" ? "Développeur Full-stack Accessibilité" : "Full-stack Developer Accessibility"}</h1>
             <div className="cv_section--head_infos-wrapper">
                 <AvailableStatusIndicator isAvailable={availableStatus} />
-                <img className="cv_section--head_identity-picture" src="/img/guitare.jpg" alt="" />
+                <img className="cv_section--head_identity-picture" src="/img/identity.jpg" alt="" />
             </div>
         </div>
     )
