ddcdde773b4ef0712d589d333f111a33986a0f60
Hugues Labat <labat.hugues@gmail.com>
2024-04-07 07:24:07 +0200

    add svg icon to warning component

--- src/Components/warning.tsx
+++ src/Components/warning.tsx
@@ -28,7 +28,11 @@ export default function Warning({frenchText,englishText}: WarningPropsInterface)
             onFocus={()=> setMessageDisplayed(true)} onMouseLeave={()=> setMessageDisplayed(false)}
             onBlur={()=> setMessageDisplayed(false)} onKeyDown={(e)=> handleKeyDown(e)}
             tabIndex={0} aria-describedby="filter-alert">
-                !!
+                <svg className="warning-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 137.47 120.39">
+                    <polygon class="cls-2" points="68.73 5 5 115.39 132.47 115.39 68.73 5"/>
+                    <line class="cls-3" x1="68.73" y1="43.41" x2="68.73" y2="76.98"/>
+                    <circle class="cls-1" cx="68.73" cy="93.84" r="6.12"/>
+                </svg>
             </span>
             {
                 messageDisplayed &&
