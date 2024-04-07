ea09bb1f3a6c1017cd9ce067e034132792cf5208
Hugues Labat <labat.hugues@gmail.com>
2024-04-07 07:24:22 +0200

    add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
 
 
