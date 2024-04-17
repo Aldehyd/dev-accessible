de6cdeb79b35b6ac215068d3ebbca6787c0cfbfb
Hugues Labat <labat.hugues@gmail.com>
2024-04-17 18:34:54 +0200

    create form main message component

---
+++ src/Components/formMainMessage.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import LanguageContext from "../Contexts/language-context.tsx";
+
+interface FormMainAlertPropsInterface {
+    role ?: string,
+    frenchText: string,
+    englishText: string,
+}
+
+export default function FormMainMessage({role="message",frenchText,englishText}: FormMainAlertPropsInterface): React.JSX.Element {
+
+    const {language} = useContext(LanguageContext);
+    const classList = `main-message ${role === "success" ? "main-success" : "main-alert"}`;
+
+    return (
+        <p className={classList} role={role === "alert" ? "alert" : "status"}>
+            {language === "french" ? frenchText : englishText}
+        </p>
+    )
+}
