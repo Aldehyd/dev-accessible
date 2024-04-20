c9ce7dc97d7fa3349c41241552a51b9159de8286
Hugues Labat <labat.hugues@gmail.com>
2024-04-20 11:26:46 +0200

    create privacy policy page

---
+++ src/Layout/privacyPolicyMain.tsx
@@ -0,0 +1,31 @@
+
+export default function PrivacyPolicy(): React.JSX.Element {
+    return (
+        <div>
+            <p>Ce site n'utilise pas de cookies.</p>
+            <p>Les seules données personnelles collectées sont l'adresse mail ainsi que les données 
+                personnelles fournies volontairement par l'utilisateur lors de l'envoi d'un message 
+                via le formulaire de la page de contact.</p>
+            <p>Ces données sont uniquement accessibles à l'administrateur du site et ne sont partagées
+                 avec personne d'autre.</p>
+            <p>Elles sont collectées dans le seul but de pouvoir répondre à la demande formulée par 
+                l'utilisateur.</p>
+            <p>Les données sont conservées pour une durée maximale de deux ans.</p>
+            <p>Conformément aux dispositions légales et réglementaires applicables, en particulier 
+                la loi no 78-17 du 6 janvier 1978 modifiée relative à l’informatique, aux fichiers 
+                et aux libertés et le règlement européen 2016/679 du 27 avril 2016 (applicable dès 
+                le 25 mai 2018), vous disposez des droits suivants :</p>
+            <ul>
+                <li>Exercer votre droit d’accès, pour connaître les données personnelles qui vous 
+                    concernent.</li>
+                <li>Demander la mise à jour de vos données, si celles-ci sont inexactes.</li>
+                <li>Demander la portabilité ou la suppression de vos données.</li>
+                <li>Demander la limitation du traitement de vos données.</li>
+                <li>Vous opposer, pour des motifs légitimes, au traitement de vos données.</li>
+            </ul>
+            <p>Vous pouvez exercer vos droits par courriel : contact@dev-accessible.com</p>
+            <p>L'utilisateur dispose également du droit d'introduire une réclamation auprès 
+                de la CNIL (Commission Nationale de l'Informatique et des Libertés).</p>
+        </div>
+    );
+};
