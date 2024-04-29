d82c7e1a607672d7940145e3dad6310b9717a519
Hugues Labat <labat.hugues@gmail.com>
2024-04-29 10:33:59 +0200

    improve accessibot style

--- src/Components/accessibot.tsx
+++ src/Components/accessibot.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Accessibot(): React.JSX.Element {
 
     const [shadowColor,setShadowColor] = useState<string>("blue");
     const [mouseOver,setMouseOver] = useState<boolean>(false);
+    const [isWaiting,setIsWaiting] = useState<boolean>(false);
 
     const onMouseOver = ()=> {
         setMouseOver(true);
@@ -14,7 +15,13 @@ export default function Accessibot(): React.JSX.Element {
         setMouseOver(false);
     };
 
-    const classList = `accessibility-bot accessibility-bot_waiting ${mouseOver ? "accessibility-bot_hover" : ""}`;
+    const classList = `accessibility-bot ${isWaiting ? "accessibility-bot_waiting" : ""} ${mouseOver ? "accessibility-bot_hover" : ""}`;
+
+    useEffect(()=> {
+        setTimeout(()=> {
+            setIsWaiting(true);
+        },2600);
+    },[]);
 
     return (
         <div className={classList} onMouseOver={()=> onMouseOver()} onMouseLeave={()=> onMouseLeave()}>
@@ -27,6 +34,7 @@ export default function Accessibot(): React.JSX.Element {
                 <div className="accessibility-bot_mouth--happy accessibility-bot_mouth"></div>
                 <div className="accessibility-bot_mouth--sad accessibility-bot_mouth"></div>
             </div>
+            <div className="accessibility-bot_shadow-appear-effect"></div>
         </div>
     )
 }
