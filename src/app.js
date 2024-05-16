e96bd55597752376099ce3bd74af2926ba0cdb53
Hugues Labat <labat.hugues@gmail.com>
2024-05-16 17:17:17 +0200

    add page-content route to app.js

--- src/app.js
+++ src/app.js
@@ -16,9 +16,9 @@ app.listen(port, ()=> {
 app.use(cors());
 app.use(express.json());
 
-const public_path = path.join(__dirname,'/dist');
+// const public_path = path.join(__dirname,'/dist');
 
-app.use('/',express.static(public_path));
+// app.use('/',express.static(public_path));
 
 app.get('/cv-status',(_,res)=>{
     GetDatasFromCollection(res,"availableStatus");
@@ -36,6 +36,10 @@ app.get('/cv-languages',(_,res)=>{
     GetDatasFromCollection(res,"languages");
 });
 
+app.get('/page-content',(req,res)=> {
+    GetPageContent(res,req.query['page']);
+});
+
 async function GetDatasFromCollection(res,collection) {
     const uri = process.env.STRING_URI;
     const client = new MongoClient(uri);
@@ -45,6 +49,23 @@ async function GetDatasFromCollection(res,collection) {
         await client.connect();
         const docs = await client.db(dbName).collection(collection).find();
         const array = await docs.toArray();
+        res.send(array)
+    } catch(err) {
+        res.send(err)
+    }  finally {
+        await client.close();
+    }
+}
+
+async function GetPageContent(res,page) {
+    const uri = process.env.STRING_URI;
+    const client = new MongoClient(uri);
+    const dbName = process.env.STRING_DBNAME;
+
+    try {
+        await client.connect();
+        const doc = await client.db(dbName).collection('pages-content').find({page: page});
+        const array = await doc.toArray();
         res.send(array)
     } catch(err) {
         res.send(err)
