9769008c4346b3591d7ad461a50913cb4482fb43
Hugues Labat <labat.hugues@gmail.com>
2024-05-17 10:21:09 +0200

    add display content to why accessibility page

--- src/Layout/whyAccessibilityMain.tsx
+++ src/Layout/whyAccessibilityMain.tsx
@@ -1,9 +1,72 @@
+import { useState, useContext, useEffect } from "react";
+import { fetchData } from "../Functions/fetchData.tsx";
 import CustomLink from "../Components/custom-link.tsx";
+import LanguageContext from "../Contexts/language-context.tsx";
+import Loader from "../Components/loader.tsx";
+import Error from "../Components/error.tsx";
 
 export default function WhyAccessibilityMain(): React.JSX.Element {
+
+    const {language} = useContext(LanguageContext);
+
+    const [content,setContent] = useState("");
+    const [isContentLoading,setIsContentLoading] = useState<boolean>(true);
+    const [error,setError] = useState<boolean>(false);
+
+    useEffect(()=> {
+        fetchData('http://localhost:4000/page-content?page=why-accessibility',setContent,setIsContentLoading,setError);
+    },[]);
+
     return (
         <>
-            <h2>Accessibilité numérique :</h2>
+        {isContentLoading && !error && <Loader />}
+            {error && <Error frenchMessage="Une erreur est survenue. Veuillez rafraichir la page svp." englishMessage="An error has occured. Please refresh the current page." />}
+            {!isContentLoading && !error &&
+                <>
+                    {content[0]?.content.map(contentUnit => {
+                        switch(contentUnit.type) {
+                            case 'title':
+                                if(contentUnit.options === 2) {
+                                    return (language === "french" ? <h2>{contentUnit.frenchContent}</h2> : <h2>{contentUnit.englishContent}</h2>)
+                                } else if(contentUnit.options === 3) {
+                                    return (language === "french" ? <h3>{contentUnit.frenchContent}</h3> : <h3>{contentUnit.englishContent}</h3>)
+                                } else if(contentUnit.options === 4) {
+                                    return (language === "french" ? <h4>{contentUnit.frenchContent}</h4> : <h4>{contentUnit.englishContent}</h4>)
+                                };
+                                break;
+                            case 'list':
+                                return <ul className={contentUnit.options === "styled" ? "styled-list" : ""}>
+                                    {
+                                        language === "french" ?
+                                            contentUnit.frenchContent.map(line => <li key={contentUnit.frenchContent.indexOf(line)}>{line}</li>)
+                                            :
+                                            contentUnit.englishContent.map(line => <li key={contentUnit.englishContent.indexOf(line)}>{line}</li>)
+                                    }
+                                </ul>
+                            case 'note':
+                                return (
+                                    language === "french" ?
+                                        <p className="note"><span>{contentUnit.frenchContent[0]}</span><span>{contentUnit.frenchContent[1]}</span></p>
+                                        :
+                                        <p className="note"><span>{contentUnit.frenchContent[0]}</span><span>{contentUnit.frenchContent[1]}</span></p>
+                                )
+                            case 'paragraph':
+                                return (
+                                    language === "french" ?
+                                        <p key={contentUnit.id}>{contentUnit.frenchContent}</p>
+                                        :
+                                        <p key={contentUnit.id}>{contentUnit.englishContent}</p>
+                                )
+                            case 'link':
+                                return <CustomLink frenchText={contentUnit.frenchText} englishText={contentUnit.englishText} route={contentUnit.route} />
+                            default:
+                                break;
+                        };
+                        
+                    })}
+                </> 
+            }
+            {/* <h2>Accessibilité numérique :</h2>
             <q>L’accessibilité numérique consiste à rendre les contenus et services numériques compréhensibles et utilisables par les personnes en situation de handicap.</q>
             <p>En France, 1 utilisateur d'internet sur 5 est en situation de handicap.</p>
             <p>L'accessibilité numérique :</p>
@@ -42,7 +105,7 @@ export default function WhyAccessibilityMain(): React.JSX.Element {
                 du RGAA (Référentiel Général d'Amélioration de l'Accessibilité).</p>
             <p>Un site est considéré accessible quand il atteint un score de 100% de conformité
                 avec ces critères.</p>
-            <CustomLink frenchText="Lien vers le RGAA" englishText="RGAA link" route="https://accessibilite.numerique.gouv.fr/" />
+            <CustomLink frenchText="Lien vers le RGAA" englishText="RGAA link" route="https://accessibilite.numerique.gouv.fr/" /> */}
         </>
     )
 }
