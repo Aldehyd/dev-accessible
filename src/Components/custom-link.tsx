0f91c3ea18f07d28d3e55f7e4f2c8dfad58b440e
Hugues Labat <labat.hugues@gmail.com>
2024-05-17 10:33:19 +0200

    add target attribute to custom link

--- src/Components/custom-link.tsx
+++ src/Components/custom-link.tsx
@@ -5,18 +5,19 @@ import LanguageContext from "../Contexts/language-context.tsx";
 interface CustomLinkInterface {
     frenchText: string,
     englishText: string,
+    openInNewTab?: boolean,
     route: string,
     type?: string
 }
 
-export default function CustomLink({frenchText,englishText,route,type="secundary"}: CustomLinkInterface): React.JSX.Element {
-
+export default function CustomLink({frenchText,englishText,openInNewTab=false,route,type="secundary"}: CustomLinkInterface): React.JSX.Element {
+    
     const {language} = useContext(LanguageContext);
 
     const classList = `link ${type === "main" ? "main-link" : ""} ${type === "secundary" ? "secundary-link" : ""}`;
 
     return(
-        <Link to={route} className={classList}>
+        <Link to={route} target={openInNewTab ? "_blank" : ""} className={classList}>
             {language === "french" ? frenchText : englishText}
         </Link>
     )   
