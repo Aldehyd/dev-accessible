428518871c73a6685be7cead62d986b4623b629a
Hugues Labat <labat.hugues@gmail.com>
2024-05-18 09:32:25 +0200

    complete accessibility analysis indicator behavior

--- src/Components/accessibility-analysis-indicator.tsx
+++ src/Components/accessibility-analysis-indicator.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import LanguageContext from "../Contexts/language-context.tsx";
 import AccessibilityAnalysisContext from "../Contexts/accessibility-analysis-context.tsx";
 
@@ -11,18 +11,39 @@ export default function AccessibilityAnalysisIndicator({setDisplay}: Accessibili
     const {language} = useContext(LanguageContext);
     const {accessibilityAnalysis,changeAccessibilityAnalysis} = useContext(AccessibilityAnalysisContext);
 
+    const [displayTip,setDisplayTip] = useState<boolean>(false);
+
     const handleClick = ()=> {
         if(accessibilityAnalysis) {
             changeAccessibilityAnalysis(false);
         };
     };
 
+    const handleMouseOver = ()=> {
+        if(accessibilityAnalysis) {
+            setDisplayTip(true);
+        };
+    };
+
+    const handleMouseLeave = ()=> {
+        if(accessibilityAnalysis) {
+            setDisplayTip(false);
+        };
+    };
+
     const classList = `accessibility-analysis-indicator ${accessibilityAnalysis ? 'on' : 'off'}`;
+
+    useEffect(()=> {
+        if(!accessibilityAnalysis) {
+            setDisplayTip(false);
+        }
+    },[accessibilityAnalysis]);
 
     return(
         <div className={classList}>
             <button type="button" className="accessibility-analysis-toggle-button" 
-                aria-disabled={accessibilityAnalysis ? "false" : "true"} onClick={()=>handleClick()}>
+                aria-disabled={accessibilityAnalysis ? "false" : "true"} onClick={()=>handleClick()}
+                onMouseOver={()=> handleMouseOver()} onMouseLeave={()=> handleMouseLeave()}>
                 {
                     accessibilityAnalysis ?
                         <span className="accessibility-analysis-toggle-button_on">
@@ -34,6 +55,17 @@ export default function AccessibilityAnalysisIndicator({setDisplay}: Accessibili
                             {language === "french" ? "Analyse accessibilité " : "Accessibility analysis "}
                             {language === "french" ? <span lang='en'>OFF</span> : "OFF"}
                         </span>
+                }
+                {
+                    displayTip && 
+                        <p className="accessibility-analysis-toggle-button_tip">
+                            {
+                                language === "french" ? 
+                                    "Cliquez pour sortir du mode analyse de l'accessibilité"
+                                    :
+                                    "Click to leave accessibility analysis mode"
+                            }
+                        </p>
                 }
             </button>
             <button type="button" aria-label={language === "french" ? "Aide" : "Help"} 
