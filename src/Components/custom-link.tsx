905c1c3d36517b852e63085e086ae3b7f96f470c
Hugues Labat <labat.hugues@gmail.com>
2024-05-18 16:31:13 +0200

    implement accessibility analysis on custom link component

--- src/Components/custom-link.tsx
+++ src/Components/custom-link.tsx
@@ -1,23 +1,57 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LanguageContext from "../Contexts/language-context.tsx";
+import AccessibilityAnalysisContext from "../Contexts/accessibility-analysis-context.tsx";
+import AccessibotCommentContext from "../Contexts/accessibot-comment-context.tsx";
 
 interface CustomLinkInterface {
     frenchText: string,
     englishText: string,
     openInNewTab?: boolean,
     route: string,
-    type?: string
+    type?: string,
+    comment?: {
+        hover: boolean,
+        compliant: boolean,
+        frenchContent: string,
+        englishContent: string
+    }
 }
 
-export default function CustomLink({frenchText,englishText,openInNewTab=false,route,type="secundary"}: CustomLinkInterface): React.JSX.Element {
+export default function CustomLink({frenchText,englishText,openInNewTab=false,route,type="secundary",comment={hover: true,compliant: true,frenchContent: "Le lien possède un nom accessible pertinent",englishContent: "Link has a pertinent accessible name"} }: CustomLinkInterface): React.JSX.Element {
     
     const {language} = useContext(LanguageContext);
+    const {accessibilityAnalysis} = useContext(AccessibilityAnalysisContext);
+    const {changeAccessibotComment} = useContext(AccessibotCommentContext);
 
-    const classList = `link ${type === "main" ? "main-link" : ""} ${type === "secundary" ? "secundary-link" : ""}`;
+    const [compliantClass,setCompliantClass] = useState<string>("");
+
+    const onAccessibilityAnalysisOver = ()=> {
+        if(accessibilityAnalysis) {
+            changeAccessibotComment(comment);
+            comment.compliant ? setCompliantClass("compliant") : setCompliantClass("not-compliant");
+        };
+    };
+
+    const onAccessibilityAnalysisLeave = ()=> {
+        if(accessibilityAnalysis) {
+            changeAccessibotComment({...comment, hover: false});
+            setCompliantClass("");
+        };
+    };
+
+    useEffect(()=> {
+        if(!accessibilityAnalysis) {
+            changeAccessibotComment({...comment, hover: false});
+            setCompliantClass("");
+        };
+    },[accessibilityAnalysis,changeAccessibotComment,comment]);
+
+    const classList = `link ${type === "main" ? "main-link" : ""} ${type === "secundary" ? "secundary-link" : ""} ${compliantClass}`;
 
     return(
-        <Link to={route} target={openInNewTab ? "_blank" : ""} className={classList}>
+        <Link to={route} target={openInNewTab ? "_blank" : ""} className={classList} 
+            onMouseOver={()=> onAccessibilityAnalysisOver()} onMouseLeave={()=> onAccessibilityAnalysisLeave()}>
             {language === "french" ? frenchText : englishText}
         </Link>
     )   
