aadd782c6b46564713803cb13d3676aac41c633b
Hugues Labat <labat.hugues@gmail.com>
2024-05-20 08:50:21 +0200

    add accessibility analysis behavior to search bar

--- src/Components/search-bar.tsx
+++ src/Components/search-bar.tsx
@@ -1,16 +1,53 @@
-import { useContext, useRef} from "react";
+import { useContext, useRef, useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import LanguageContext from "../Contexts/language-context.tsx";
 import EnvironnementContext from "../Contexts/environnement-context.tsx";
+import AccessibilityAnalysisContext from "../Contexts/accessibility-analysis-context.tsx";
+import AccessibotCommentContext from "../Contexts/accessibot-comment-context.tsx";
+import AccessibilityAnalysisWarningContext from "../Contexts/accessibility-analysis-warning-context.tsx";
+interface SearchBarPropsInterface {
+    comment?: {
+        hover: boolean,
+        compliant: boolean,
+        frenchContent: string,
+        englishContent: string
+    }
+}
 
-export default function SearchBar(): React.JSX.Element {
+export default function SearchBar({comment={hover: true,compliant: true,frenchContent: "Le bouton possède un nom accessible via l'attribut 'aria-label'",englishContent: "The button has an accessible name thanks to the 'aria-label' attribute"}}:SearchBarPropsInterface): React.JSX.Element {
 
     const {language} = useContext(LanguageContext);
     const {environnement} = useContext(EnvironnementContext);
+    const {accessibilityAnalysis} = useContext(AccessibilityAnalysisContext);
+    const {changeAccessibotComment} = useContext(AccessibotCommentContext);
+    const {changeAccessibilityAnalysisWarning} = useContext(AccessibilityAnalysisWarningContext);
 
     const navigate = useNavigate();
 
     const input = useRef(null);
+
+    const [compliantClass,setCompliantClass] = useState<string>("");
+
+    const onAccessibilityAnalysisOver = ()=> {
+        if(accessibilityAnalysis) {
+            changeAccessibotComment(comment);
+            comment.compliant ? setCompliantClass("compliant") : setCompliantClass("not-compliant");
+        };
+    };
+
+    const onAccessibilityAnalysisLeave = ()=> {
+        if(accessibilityAnalysis) {
+            changeAccessibotComment({...comment, hover: false});
+            setCompliantClass("");
+        };
+    };
+
+    useEffect(()=> {
+        if(!accessibilityAnalysis) {
+            changeAccessibotComment({...comment, hover: false});
+            setCompliantClass("");
+        };
+    },[accessibilityAnalysis]);
 
     const handleKeyDown = (e: KeyboardEvent)=> {
         switch(e.key) {
@@ -23,10 +60,14 @@ export default function SearchBar(): React.JSX.Element {
     };
 
     const launchSearch = ()=> {
-        if(input.current?.value.length > 0) {
+        if(input.current?.value.length > 0 && !accessibilityAnalysis) {
             navigate('/search-results',{state: {language: language, environnement: environnement, query: input.current?.value}})
+        } else {
+            changeAccessibilityAnalysisWarning(true);
         };
     };
+
+    const classList = `search-button ${compliantClass}`;
 
     return(
         <div className="search-bar-container">
@@ -34,9 +75,10 @@ export default function SearchBar(): React.JSX.Element {
                 <input type="text" placeholder={language === "french" ? "Rechercher..." : "Search..."}
                  maxLength={26} onKeyDown={(e)=> handleKeyDown(e)} ref={input} />
             </span>
-            <button type="button" className="search-button" 
+            <button type="button" className={classList}
                 aria-label={language === "french" ? "Rechercher" : "Search"}
-                onClick={()=> launchSearch()}>
+                onClick={()=> launchSearch()}
+                onMouseOver={()=> onAccessibilityAnalysisOver()} onMouseLeave={()=> onAccessibilityAnalysisLeave()}>
                 <svg className="magnifying-glass-svg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 97.8 96.3">
                     <line className="st0" x1="-177" y1="10.5" x2="-120.5" y2="67"/>
                     <line className="st0" x1="-120.5" y1="10.5" x2="-177" y2="67"/>
